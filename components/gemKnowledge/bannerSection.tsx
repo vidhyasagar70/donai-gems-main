c0cf5d299309de149ff16cf6254f77198a093f35
vidhyasagar70 <vidhyas369@gmail.com>
2025-10-04 20:23:14 +0530

    bannersection

--- components/gemKnowledge/bannerSection.tsx
+++ components/gemKnowledge/bannerSection.tsx
@@ -1,21 +1,23 @@
+'use client';
+
 import Image from 'next/image';
 
 export default function BannerSection() {
   return (
-    <div className="relative w-full h-64 md:h-80 lg:h-96">
-      
+    <div className="relative w-full h-64 md:h-80 lg:h-96 overflow-hidden">
+      {/* Background Image */}
       <Image
-        src="/images/bannerForGem.png"
+        src="/images/bannerForGem.png" 
         alt="Banner background"
         fill
-        className="object-cover"
         priority
         sizes="100vw"
+        className="object-cover object-center"
       />
-      
+
       {/* Dark overlay */}
-      <div className="absolute inset-0 bg-black bg-opacity-40 z-10"></div>
-      
+      <div className="absolute inset-0 bg-black/40 z-10"></div>
+
       {/* Centered Text */}
       <div className="relative z-20 flex items-center justify-center h-full">
         <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-white text-center px-4">
